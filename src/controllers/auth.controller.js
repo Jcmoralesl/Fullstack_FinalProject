48ca618a69d815d2d5a9a0d080870f5d2af22a54
Jcmoralesl <juancamilo.morales43@gmail.com>
2023-09-24 22:21:39 -0500

    Login y inicios de logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,19 +1,67 @@
 import User from '../models/user.model.js'
+import bcrypt from 'bcryptjs'
+import { createAccessToken } from '../libs/jwt.js'
+
+
+// REGISTER 
+
 export const register = async (req, res) => {
     const {email, password, username} = req.body
 
     try {
+
+        const passwordHash = await bcrypt.hash(password, 10)
+
         const newUser = new User ({
             username,
             email,
-            password
+            password: passwordHash,
         })
 
         const userSaved = await newUser.save()
-        res.json(userSaved)
+
+        const token = await createAccessToken({id: userSaved._id})
+        res.cookie('token', token)
+        res.json({
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+            createAt: userSaved.createdAt,
+            updateAt: userSaved.updatedAt,
+            });
     } catch (error) {
-        console.log(error)
+        res.status(500).json({message: error.message})
     }
 }
-export const login = (req, res) => res.send ('login')
 
+// LOGIN 
+
+export const login = async (req, res) => {
+    const {email, password} = req.body
+
+    try {
+        const userFound = await User.findOne ({email})
+        if (!userFound) return res.status(400).json({message: 'User not found'});
+
+        const isMatch = await bcrypt.compare(password, userFound.password)
+
+        if (!isMatch) return res.status(400).json ({message: "Incorrect password"})
+
+        const token = await createAccessToken({id: userFound._id})
+
+        res.cookie('token', token)
+        res.json({
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email,
+            createAt: userFound.createdAt,
+            updateAt: userFound.updatedAt,
+            });
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
+
+// LOGOUT 
+
+export const logout = async (req, res) => {}
