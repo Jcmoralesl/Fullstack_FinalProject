7ee4e5fbb18b56c0a0a9ea3626a1b06f64e1e78d
Jcmoralesl <juancamilo.morales43@gmail.com>
2023-09-25 20:15:51 -0500

    Logout bakend

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -64,4 +64,9 @@ export const login = async (req, res) => {
 
 // LOGOUT 
 
-export const logout = async (req, res) => {}
+export const logout = async (req, res) => {
+    res.cookie('token', "", {
+        expires: new Date(0)
+    })
+    return res.sendStatus(200)
+}
