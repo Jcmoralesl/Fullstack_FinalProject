f2488ee65534cba05b0907d78d638b2a1cfc97f0
Jcmoralesl <juancamilo.morales43@gmail.com>
2023-09-27 21:29:12 -0500

    Inicio pagina registro

--- Client/src/views/registerPage.jsx
+++ Client/src/views/registerPage.jsx
@@ -1,6 +1,26 @@
+import { useForm } from "react-router-dom"
+
 function RegisterPage () {
+
+    const {register, handleSubmit} = useForm()
+
     return (
-        <div>Register Page</div>
+        <div>
+
+            <form onSubmit={handleSubmit(values => {
+                console.log(values)
+            })}>
+
+                <input type="text" {...register("username", {required: true})}/>
+                <input type="email" {... register("email", {required: true})}/>
+                <input type="password" {... register("password", {required: true})}/>
+                <button type="submit">
+                    Register                    
+                </button>
+            </form>
+
+
+        </div>
     )
 }
 
