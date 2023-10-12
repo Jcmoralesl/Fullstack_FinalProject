186c8328164c1eca4f46f409244e797a62f3ef1d
Jcmoralesl <juancamilo.morales43@gmail.com>
2023-10-11 21:54:21 -0500

    Cambio de imagen note a public

--- Client/src/views/homePage.jsx
+++ Client/src/views/homePage.jsx
@@ -1,7 +1,7 @@
 function HomePage () {
     return (
         <div className='flex h-[calc(100vh-100px)] items-center justify-center'>
-            <img src= "notas.png" className="my-10" alt="Logo" />
+            <img src= "/notas.png" className="my-10" alt="Logo" />
         <div> 
             <h1 className="text-teal-700 text-xl font-bold my-2">WELCOME TO MEMORIUM ALERT</h1></div>
         </div>
