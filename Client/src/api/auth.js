08c11fa5712b344603c30f3c7144066a96856036
Jcmoralesl <juancamilo.morales43@gmail.com>
2023-10-11 21:57:15 -0500

    enlace backend en login

--- Client/src/api/auth.js
+++ Client/src/api/auth.js
@@ -2,6 +2,6 @@ import axios from "./axios";
 
 export const registerRequest = user => axios.post(`/register`, user);
 
-export const  loginRequest = user => axios.post(`/login`, user)
+export const  loginRequest = user => axios.post(`https://notes-backend-nqp8.onrender.com/api/login`, user)
 
 export const verifyTokenRequest = () => axios.get('/verify')
