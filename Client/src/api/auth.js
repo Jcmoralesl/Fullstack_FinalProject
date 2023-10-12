775742d512d927a5aad92880e45b37af335bd11b
Jcmoralesl <juancamilo.morales43@gmail.com>
2023-10-11 21:59:01 -0500

    Enlace backend en register y verify

--- Client/src/api/auth.js
+++ Client/src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from "./axios";
 
-export const registerRequest = user => axios.post(`/register`, user);
+export const registerRequest = user => axios.post(`https://notes-backend-nqp8.onrender.com/api/register`, user);
 
 export const  loginRequest = user => axios.post(`https://notes-backend-nqp8.onrender.com/api/login`, user)
 
-export const verifyTokenRequest = () => axios.get('/verify')
+export const verifyTokenRequest = () => axios.get('https://notes-backend-nqp8.onrender.com/api/verify')
